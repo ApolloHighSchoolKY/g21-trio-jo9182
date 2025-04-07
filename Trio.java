1d2f9648e6adaed3f0836dc0ed1b9fc12324c5d2
jo9182 <73309930+jo9182@users.noreply.github.com>
2025-04-07 14:27:31 +0000

    food

--- Trio.java
+++ Trio.java
@@ -1,1 +1,31 @@
 //Write your answer here
+public class Trio implements MenuItem{
+    Sandwich sandwich;
+    Salad salad;
+    Drink drink;
+
+    public Trio(){
+
+    }
+
+    public Trio(Sandwich sand, Salad sald, Drink dink){
+        sandwich = sand;
+        salad = sald;
+        drink = dink;
+    }
+
+    public String getName(){
+        return sandwich.getName() + " / " + salad.getName() + " / " + drink.getName() + " Trio";
+    }
+
+    public double getPrice(){
+        if(salad.getPrice() < sandwich.getPrice && salad.getPrice() < drink.getPrice)
+            return sandwich.getPrice() + drink.getPrice();
+
+        if(drink.getPrice() < sandwich.getPrice && drink.getPrice() < salad.getPrice)
+            return sandwich.getPrice() + salad.getPrice();
+        
+        return drink.getPrice() + salad.getPrice();
+
+    }
+}
